a82ba4e380c544bcb272cfeb52d75536d09a1c1d
tranthientam293 <tranthientam2903@gmail.com>
2025-04-01 13:25:27 +0000

    update .gitignore file, plugins

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,4 +1,12 @@
 return {
-  'nvim-lua/plenary.nvim', -- lua functions that many plugins use
-  'christoomey/vim-tmux-navigator', -- tmux & split window navigation
+  {
+    'nvim-lua/plenary.nvim', -- lua functions that many plugins use
+    'christoomey/vim-tmux-navigator', -- tmux & split window navigation
+  },
+  {
+    'norcalli/nvim-colorizer.lua',
+    config = function()
+      require('colorizer').setup({'*'})
+    end
+  }
 }
